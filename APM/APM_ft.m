341e1bb88dd49d16b52560b2eef5f76258044130
Wang1lan <127758159+Wang1lan@users.noreply.github.com>
2026-09-21 17:17:04 +0800

    Add files via upload

---
+++ APM/APM_ft.m
@@ -0,0 +1,14 @@
+function ImgOut = CCM_ft(imgin, ccm)
+
+ImgOut = imgin;
+
+r = imgin(:,:,1);
+g = imgin(:,:,2);
+b = imgin(:,:,3);
+
+ImgOut(:,:,1) = ccm(1,1) * r + ccm(1,2) * g + ccm(1,3) * b;
+ImgOut(:,:,2) = ccm(2,1) * r + ccm(2,2) * g + ccm(2,3) * b;
+ImgOut(:,:,3) = ccm(3,1) * r + ccm(3,2) * g + ccm(3,3) * b;
+
+ImgOut(ImgOut < 0) = 0;
+ImgOut(ImgOut > 2^0 - 2^(-13)) = 2^0 - 2^(-13);
